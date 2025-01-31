1bc10eaae364e68dd539b86eb782ea01a086b182
mahowak <kmahowald@gmail.com>
2025-01-30 20:52:01 -0600

    bring up analysis

---
+++ analysis/prepare_for_regression.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+
+    do_scores = pd.read_csv('test_dos.csv')
+    po_scores = pd.read_csv('test_pos.csv')
+
+    all_data = pd.concat([do_scores, po_scores], ignore_index=True)
+    all_data['recipient_length'] = all_data['recipient'].apply(
+            lambda x: len([word for word in x.split() if any(char.isalnum() for char in word)])
+        )    
+    all_data['theme_length'] = all_data['theme'].apply(
+            lambda x: len([word for word in x.split() if any(char.isalnum() for char in word)])
+        ) 
+    all_data['length_difference'] = np.log(all_data['recipient_length'] / all_data['theme_length'])
+
+    all_data['recipient_pronoun'] = np.where(all_data['recipient_pos'] == 'PRON', 0.5, -0.5)
+    all_data['theme_pronoun'] = np.where(all_data['theme_pos'] == 'PRON', 0.5, -0.5)
+    all_data.to_csv("all_data.csv", index=False)
+
+
+
