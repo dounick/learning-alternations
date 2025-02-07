0172ac709e1c5cd984de74a296a337ad771c6dd4
Kanishka Misra <menogetusername@gmail.com>
2025-02-07 12:04:54 -0600

    new plot

---
+++ analysis/hawkins-plots.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+library(ggtext)
+
+hawkins_raw <- read_csv("analysis/hawkins/results.csv")
+
+results <- hawkins_raw %>%
+  filter(verb_count >= 1) %>%
+  select(i,verb_id, classification, behavior=BehavDOpreference, gpt2_ratio, default_ratio, balanced_ratio) %>% 
+  mutate(
+    behavior = behavior/100,
+    behavior = behavior - min(behavior)/(max(behavior) - min(behavior)),
+    gpt2_ratio = gpt2_ratio - min(gpt2_ratio)/(max(gpt2_ratio) - min(gpt2_ratio)),
+    default_ratio = default_ratio - min(default_ratio)/(max(default_ratio) - min(default_ratio)),
+    balanced_ratio = balanced_ratio - min(balanced_ratio)/(max(balanced_ratio) - min(balanced_ratio)),
+  ) %>%
+  group_by(verb_id, classification) %>%
+  summarize(
+    behavior = mean(behavior),
+    gpt2_ratio = mean(gpt2_ratio),
+    default_ratio = mean(default_ratio),
+    balanced_ratio = mean(balanced_ratio)
+  ) %>%
+  ungroup()
+  
+  
+
+rho <- results %>%
+  summarize(
+    gpt2 = cor(gpt2_ratio, behavior, method = "pearson"),
+    default = cor(default_ratio, behavior, method = "pearson"),
+    balanced = cor(balanced_ratio, behavior, method = "pearson"),
+  ) %>%
+  pivot_longer(gpt2:balanced, names_to = "condition", values_to = "r") %>%
+  mutate(
+    x = 0.6, y = case_when(condition == "gpt2" ~ -5, TRUE ~ -1),
+    r = paste("<i>r</i> =", round(r, 2))
+  ) %>%
+  mutate(condition = factor(condition, levels = c("default", "balanced", "gpt2"), labels = c("Unablated BabyLM", "Balanced (Strict)", "GPT-2 Small")))
+  
+
+results %>%
+  pivot_longer(gpt2_ratio:balanced_ratio, names_to = "condition", values_to = "do_pref") %>%
+  mutate(condition = str_remove(condition, "_ratio")) %>%
+  # mutate(condition = factor(condition, levels = c("default", "balanced"), labels = c("Strict (unablated)", "Strict (balanced)"))) %>%
+  mutate(condition = factor(condition, levels = c("default", "balanced", "gpt2"), labels = c("Unablated BabyLM", "Balanced (Strict)", "GPT-2 Small"))) %>%
+  filter(condition %in% c("Unablated BabyLM", "GPT-2 Small")) %>%
+  mutate(classification = case_when(classification == "alternating" ~ "Alternating", TRUE ~ "Non-alternating")) %>%
+  ggplot(aes(behavior, do_pref, color = classification)) +
+  geom_point(size = 2, alpha = 0.6) +
+  geom_smooth(aes(group=NA), method = "lm", color = "black") +
+  geom_richtext(
+    data=rho %>% filter(condition %in% c("Unablated BabyLM", "GPT-2 Small")), 
+    aes(x=x, y=y, label=r), size=4, family="Times", color = "black"
+  ) +
+  facet_wrap(~ condition, scales = "free_y") +
+  scale_color_manual(values = c("#018571", "#a6611a")) +
+  theme_bw(base_size = 16, base_family="Times") +
+  theme(
+    legend.position = "top",
+    panel.grid = element_blank()
+  ) +
+  labs(
+    x = "Human Judgment",
+    y = "DO preference",
+    color = "Classification"
+  )
+
+ggsave("paper/hawkins-comparison.pdf", height=3.8, width=6.30, dpi=300, device=cairo_pdf)
---
+++ analysis/klm-plots.R
@@ -0,0 +1,99 @@
+library(tidyverse)
+library(ggtext)
+
+d = read_csv("analysis/all_data.csv") %>%
+  select(global_idx, recipient_pronoun, theme_pronoun,
+         loose_default_ratio:ditransitives_removed_ratio,
+         length_difference,
+         verb_lemma) %>%
+  pivot_longer(cols=loose_default_ratio:ditransitives_removed_ratio, names_to = "condition", values_to = "score") %>% 
+  # pivot_longer(cols = `babylm-default_ratio`:`long_first_nodatives_ratio`, names_to=c("variable")) %>%
+  mutate(recipient_pronoun = ifelse(recipient_pronoun > 0, "pronoun", "NP")) %>%
+  mutate(condition = gsub("_ratio", "", condition)) %>%
+  mutate(
+    condition = factor(
+      condition, 
+      levels = c("loose_default", "loose_balanced", "default", "balanced", "datives_removed", "ditransitives_removed", "short_first_nodatives", "random_first_nodatives", "long_first_nodatives"),
+      labels = c("Unablated (Strict)", "Balanced (Loose)", "Unablated (Strict)", 
+                 "Balanced (Strict)", "No Datives", "No Ditransitives", "Short-first (No Datives)", "Random-first (No Datives)", "Long-first (No Datives)")
+    )
+  )
+
+d %>% count(condition)
+
+# chosen.levels = c("babylm-default",
+#                   "babylm-balanced",
+#                   "loose-balanced",
+#                   "loose-default",        
+#                   "ditransitives_removed",
+#                   "datives_removed",
+#                   "short_first_nodatives",
+#                   "long_first_nodatives")
+
+# Kyle's first plot
+slopes = d %>%
+  group_by(condition, recipient_pronoun) %>%
+  summarise(slope = cor(score, length_difference)) %>%
+  mutate(x = -3, y=ifelse(recipient_pronoun == "NP", 1, .5),
+         val = as.character(round(slope, 2)))
+
+# Main exp plots
+slopes2 = d %>%
+  group_by(condition) %>%
+  summarise(slope = cor(score, length_difference)) %>%
+  mutate(x = -2, y=1.5,
+         val = paste("<i>r</i> =",as.character(format(round(slope, 2), nsmall=2))))
+
+# plot2_data = d %>%
+#   group_by(length_difference, condition, recipient_pronoun) %>%
+#   summarise(m = mean(score), .groups = "drop")
+# 
+# plot2_data$condition = factor(plot2_data$condition,
+#                              levels = unique(d$condition))
+# 
+# ggplot(plot2_data, aes(x = length_difference, y = m, colour = recipient_pronoun)) +
+#   geom_point() +
+#   theme_bw(base_size = 14) +
+#   facet_wrap(~factor(condition, levels = unique(d$condition)), nrow = 1) +
+#   ylab("P(DO alternant) - P(PO alternant)") +
+#   geom_smooth(method = "lm") +
+#   scale_colour_manual(values = c("black", "darkorange")) +
+#   geom_text(data=slopes, 
+#             aes(x=x, y=y, label=val), size=3)
+
+# Second plot (without recipient_pronoun as a factor)
+plot2_data = d %>%
+  group_by(length_difference, condition) %>%
+  summarise(m = mean(score), .groups = "drop")
+
+# plot2_data$condition = factor(plot2_data$condition,
+#                              levels = unique(d$condition))
+
+
+plot2_data %>%
+  # filter(condition %in% c("Strict (unablated)", "Strict (balanced)", "Short-first (No Datives)", "Long-first (No Datives)")) %>%
+  filter(!condition %in% c("Unablated (Loose)", "Balanced (Loose)")) %>%
+  ggplot(aes(x = length_difference, y = m)) +
+    geom_point(size = 2, alpha = 0.5, color = "black") +
+    facet_wrap(~condition, nrow = 1) +
+    ylab("P(DO alternant) - P(PO alternant)") +
+    xlab("log(len(recipient)) - log(len(theme))") + 
+    geom_smooth(method = "lm") +
+    # scale_colour_manual(values = c("black", "darkorange")) +
+    geom_richtext(
+      data=slopes2 %>% filter(!condition %in% c("Unablated (Loose)", "Balanced (Loose)")), 
+      aes(x=x, y=y, label=val), size=4, family="CMU Serif", color = "black",
+      fill="cornsilk"
+    ) +
+  theme_bw(base_size = 16, base_family = "Times") +
+  theme(
+    panel.grid = element_blank(),
+    axis.text = element_text(color = "black")
+  )+
+    labs(
+      y = "DO Preference",
+      x = "Log Difference in Recipient and Theme Lengths"
+    )
+
+
+
