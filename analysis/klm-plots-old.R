2322d565e2eab4e335e053e47119ad339af4a958
Kanishka Misra <menogetusername@gmail.com>
2025-02-09 17:34:24 -0600

    new plots with smol data

--- analysis/klm-plots-old.R
+++ analysis/klm-plots-old.R
@@ -5,18 +5,17 @@ rawdata <- read_csv("analysis/all_data.csv")
 
 d =  rawdata %>%
   select(global_idx, recipient_pronoun, theme_pronoun, recipient_anim,
-         datives_removed_ratio:long_first_small_ratio,
+         loose_default_ratio:ditransitives_removed_ratio,
          length_difference,
          verb_lemma) %>%
-  pivot_longer(cols=datives_removed_ratio:long_first_small_ratio, names_to = "condition", values_to = "score") %>% 
+  pivot_longer(cols=loose_default_ratio:ditransitives_removed_ratio, names_to = "condition", values_to = "score") %>% 
   # pivot_longer(cols = `babylm-default_ratio`:`long_first_nodatives_ratio`, names_to=c("variable")) %>%
   mutate(recipient_pronoun = ifelse(recipient_pronoun > 0, "pronoun", "NP")) %>%
-  # mutate(condition = gsub("small_ratio", "", condition)) %>%
-  mutate(condition = str_replace(condition, "(_small)?_ratio", "")) %>%
+  mutate(condition = gsub("_ratio", "", condition)) %>%
   mutate(
     condition = factor(
       condition, 
-      levels = c("loose_default", "loose_balanced", "default", "balanced", "datives_removed", "ditransitives_removed", "short_first", "random_first", "long_first"),
+      levels = c("loose_default", "loose_balanced", "default", "balanced", "datives_removed", "ditransitives_removed", "short_first_nodatives", "random_first_nodatives", "long_first_nodatives"),
       labels = c("Unablated (Loose)", "Balanced (Loose)", "Unablated (Strict)", 
                  "Balanced (Strict)", "No Datives", "No Ditransitives", "Short-first (No Datives)", "Random-first (No Datives)", "Long-first (No Datives)")
     )
@@ -34,17 +33,17 @@ d %>% count(condition)
 #                   "long_first_nodatives")
 
 # Kyle's first plot
-# slopes = d %>%
-#   group_by(condition, recipient_pronoun) %>%
-#   summarise(slope = cor(score, length_difference)) %>%
-#   mutate(x = -3, y=ifelse(recipient_pronoun == "NP", 1, .5),
-#          val = as.character(round(slope, 2)))
+slopes = d %>%
+  group_by(condition, recipient_pronoun) %>%
+  summarise(slope = cor(score, length_difference)) %>%
+  mutate(x = -3, y=ifelse(recipient_pronoun == "NP", 1, .5),
+         val = as.character(round(slope, 2)))
 
 # Main exp plots
 slopes2 = d %>%
   group_by(condition) %>%
   summarise(slope = cor(score, length_difference)) %>%
-  mutate(x = -1.85, y=1.35,
+  mutate(x = -2, y=1.5,
          val = paste("<i>r</i> =",as.character(format(round(slope, 2), nsmall=2))))
 
 # plot2_data = d %>%
@@ -75,31 +74,30 @@ plot2_data = d %>%
 
 plot2_data %>%
   # filter(condition %in% c("Strict (unablated)", "Strict (balanced)", "Short-first (No Datives)", "Long-first (No Datives)")) %>%
-  filter(!condition %in% c("Unablated (Strict)", "Balanced (Strict)")) %>%
+  filter(!condition %in% c("Unablated (Loose)", "Balanced (Loose)")) %>%
   ggplot(aes(x = length_difference, y = m)) +
-    geom_point(size = 2, alpha = 0.5, color = "black") +
-    facet_wrap(~condition, nrow = 1) +
-    ylab("P(DO alternant) - P(PO alternant)") +
-    xlab("log(len(recipient)) - log(len(theme))") + 
-    geom_smooth(method = "lm") +
-    # scale_colour_manual(values = c("black", "darkorange")) +
-    geom_richtext(
-      data=slopes2 %>% filter(!condition %in% c("Unablated (Strict)", "Balanced (Strict)")), 
-      aes(x=x, y=y, label=val), size=4, family="CMU Serif", color = "black",
-      fill="cornsilk"
-    ) +
+  geom_point(size = 2, alpha = 0.5, color = "black") +
+  facet_wrap(~condition, nrow = 1) +
+  ylab("P(DO alternant) - P(PO alternant)") +
+  xlab("log(len(recipient)) - log(len(theme))") + 
+  geom_smooth(method = "lm") +
+  # scale_colour_manual(values = c("black", "darkorange")) +
+  geom_richtext(
+    data=slopes2 %>% filter(!condition %in% c("Unablated (Loose)", "Balanced (Loose)")), 
+    aes(x=x, y=y, label=val), size=4, family="CMU Serif", color = "black",
+    fill="cornsilk"
+  ) +
   theme_bw(base_size = 16, base_family = "Times") +
   theme(
     panel.grid = element_blank(),
-    strip.text = element_text(size = 15),
     axis.text = element_text(color = "black")
   )+
-    labs(
-      y = "DO Preference",
-      x = "Log Difference in Recipient and Theme Lengths"
-    )
+  labs(
+    y = "DO Preference",
+    x = "Log Difference in Recipient and Theme Lengths"
+  )
 
-ggsave("paper/length-pref.pdf", dpi=300, height=3.80, width=14.10, device=cairo_pdf)
+ggsave("paper/length-pref.pdf", dpi=300, height=4.34, width=15.20, device=cairo_pdf)
 
 
 animacy_data <- d %>%
@@ -181,8 +179,8 @@ animacy_data %>%
     x = "Recipient Animacy",
     y = "DO Preference"
   )
-  # distinct(global_idx, recipient_anim) %>% 
-  # count(recipient_anim)
+# distinct(global_idx, recipient_anim) %>% 
+# count(recipient_anim)
 
 ggsave("paper/animacy.pdf", height=3, width=9.5, dpi=300, device=cairo_pdf)
 
