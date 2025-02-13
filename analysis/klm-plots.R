cb93a2a782d7912c398c9e2b091d189eccdc9567
mahowak <kmahowald@gmail.com>
2025-02-12 20:37:00 -0600

    add regressions

--- analysis/klm-plots.R
+++ analysis/klm-plots.R
@@ -3,14 +3,18 @@ library(ggtext)
 
 rawdata <- read_csv("analysis/all_data.csv")
 
+filter(rawdata, is.na(recipient_pos_spacy)) %>%
+  select(sentence, recipient_pos, recipient_pos_spacy)
+
 d =  rawdata %>%
-  select(global_idx, recipient_pos, recipient_anim,
+  select(global_idx, recipient_pos, recipient_anim, theme_pos, theme_anim,
          datives_removed_ratio:long_first_noditransitive_ratio,
          length_difference,
          verb_lemma) %>%
   pivot_longer(cols=datives_removed_ratio:long_first_noditransitive_ratio, names_to = "condition", values_to = "score") %>% 
   # pivot_longer(cols = `babylm-default_ratio`:`long_first_nodatives_ratio`, names_to=c("variable")) %>%
-  mutate(recipient_pos = ifelse(recipient_pos == "PRON", "pronoun", "NP")) %>%
+  mutate(recipient_pos = ifelse(recipient_pos == "PRON" | is.na(recipient_pos), "pronoun", "NP"),
+         theme_pos = ifelse(theme_pos == "PRON" | is.na(theme_pos), "pronoun", "NP")) %>%
   # mutate(condition = gsub("small_ratio", "", condition)) %>%
   mutate(condition = str_replace(condition, "(_small)?_ratio", "")) %>%
   mutate(
@@ -25,6 +29,7 @@ d =  rawdata %>%
 
 
 d %>% count(condition)
+
 
 # chosen.levels = c("babylm-default",
 #                   "babylm-balanced",
@@ -190,4 +195,121 @@ animacy_data %>%
 
 # ggsave("paper/animacy.pdf", height=3, width=9.5, dpi=300, device=cairo_pdf)
 ggsave("paper/animacy.pdf", height=6.42, width=7.85, dpi=300, device=cairo_pdf)
+
+
+
+#################
+library(lme4)
+
+# just unablated
+d$Animacy = ifelse(d$recipient_anim == "a", .5, -.5)
+d$recipient_animacy = ifelse(d$recipient_anim == "i", -.5, .5)
+d$theme_animacy = ifelse(d$theme_anim == "i", -.5, .5)
+d$theme_pos = ifelse(d$theme_pos == "pronoun", .5, -.5)
+d$recipient_pos = ifelse(d$recipient_pos == "pronoun", .5, -.5)
+
+d$animacy_contrast = ifelse(d$recipient_animacy == d$theme_animacy, 0,
+                            ifelse(d$recipient_animacy > d$theme_animacy, 1, -1))
+
+d$pos_contrast = ifelse(d$recipient_pos == d$theme_pos, 0,
+                        ifelse(d$recipient_pos > d$theme_pos, 1, -1))
+###############################################
+
+x = NULL
+for (i in unique(d$condition)) {
+  print(i)
+  l =lmerTest::lmer(data=filter(d, condition == i),
+          score ~ 
+            length_difference + animacy_contrast + pos_contrast + 
+            (1 + length_difference + animacy_contrast + pos_contrast||verb_lemma),
+          REML=F)
+  a = anova(l)
+  print(a)
+ x = rbind(x, c(i, round(fixef(l)[2], 2),
+                round(fixef(l)[3], 2),
+                round(fixef(l)[4], 2),
+                round( a$`Pr(>F)`[1], 10),
+                round( a$`Pr(>F)`[2], 10),
+                round( a$`Pr(>F)`[3], 10)
+  ))
+  print(i)
+  print(anova(l))
+}
+a = data.frame(x) 
+names(a) = c("condition", "length.coef", "anim.coef", "pron.coef", "length.p", "anim.p", "pron.p")
+
+a$length.p = as.numeric(a$length.p)
+a$anim.p = as.numeric(a$anim.p)
+a$pron.p = as.numeric(a$pron.p)
+# Function to format coefficients with significance
+
+format_coef <- function(coef, pval) {
+  sig <- ifelse(pval < 0.05, "*", "")
+  paste0(coef, sig)
+}
+
+# Apply formatting
+df <- a %>%
+  mutate(
+    length.coef = mapply(format_coef, length.coef, length.p),
+    anim.coef = mapply(format_coef, anim.coef, anim.p),
+    pron.coef = mapply(format_coef, pron.coef, pron.p)
+  ) %>%
+  select(condition, length.coef, anim.coef, pron.coef)  # Drop p-values
+
+# Print as LaTeX table
+print(xtable(df, caption = "Regression Coefficients with Significance", align = c("l", "l", "c", "c", "c")),
+      include.rownames = FALSE, sanitize.text.function = function(x) x)
+
+
+#################
+compare.remove = filter(d, condition %in% c("Unablated (Strict)", "No Datives", "No Ditransitives"))
+compare.remove$condition = as.factor(as.character(compare.remove$condition))
+compare.remove$condition = factor(compare.remove$condition, 
+                                  levels=c("Unablated (Strict)", "No Datives", "No Ditransitives"))
+
+l.remove =lmerTest::lmer(data=compare.remove,
+        score ~ 
+          length_difference*condition + 
+          animacy_contrast*condition + 
+          pos_contrast*condition + 
+          (1 + length_difference + animacy_contrast + pos_contrast ||verb_lemma),
+        REML=F)
+summary(l.remove)
+anova(l.remove)
+
+#########################
+balance = filter(d, condition %in% c("Unablated (Loose)", "Balanced (Loose)"))
+balance$condition = as.factor(as.character(balance$condition))
+balance$condition = factor(balance$condition, 
+                                  levels=c("Unablated (Loose)", "Balanced (Loose)"))
+
+l.balance =lmerTest::lmer(data=balance,
+               score ~ 
+                 length_difference*condition + 
+                 animacy_contrast*condition + 
+                 pos_contrast*condition + 
+                 (1 + length_difference + animacy_contrast + pos_contrast ||verb_lemma),
+               REML=F)
+summary(l.balance)
+anova(l.balance)
+
+#########################################################################
+ords = c("Short-first\n(No Ditransitives)",
+         "Random-first\n(No Ditransitives)",
+         "Long-first\n(No Ditransitives)")
+gram = filter(d, condition %in% ords)
+gram$condition = as.factor(as.character(gram$condition))
+
+l.gram =lmerTest::lmer(data=gram,
+                score ~ 
+                  length_difference*condition + 
+                  animacy_contrast*condition + 
+                  pos_contrast*condition + 
+                  (1 + length_difference + animacy_contrast + pos_contrast ||verb_lemma),
+                REML=F)
+anova(l.gram)
+summary(l.gram)
+
+
 
